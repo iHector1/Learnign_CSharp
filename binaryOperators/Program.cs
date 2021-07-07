e49618100d08a2cacb8ff14c8453adf82e6be0d5
iHector1 <hectorjosuegc@gmail.com>
2021-07-07 18:56:07 -0500

    BinaryOperations

---
+++ binaryOperators/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using static System.Console;
+namespace binaryOperators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 6;
+            WriteLine($"a={a}");
+            WriteLine($"b={b}");
+            WriteLine($"a & b ={a & b}");
+            WriteLine($"a & b ={a & b}");
+            WriteLine($"a | b ={a | b}");
+            WriteLine($"a ^ b ={a ^ b}");
+
+
+            #region Byinary shift
+            WriteLine($"a<<3={a<<3}");
+            WriteLine($"a*8 ={a * 8}");
+            WriteLine($"b>>1={b >> 1}");
+            #endregion
+
+            int age = 47;
+           // char fistDigit = age.ToString();
+        }
+    }
+}
